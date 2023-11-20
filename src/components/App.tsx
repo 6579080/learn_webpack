2004d6a34a921b35e6d2c2375b9b6643f2cd6578
Ulad <6579080@gmail.com>
2023-11-20 17:47:25 +0300

    6

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
-import './App.scss'
+import classes from './App.module.scss';
+
 
 export const App =() =>{
     const [count, setCount] = useState<number>(0);
     const increment = () => setCount(prev => prev +1)
     return(
         <div>
-            <h1>{count}</h1>
-            <button onClick={increment}><span>
+            <h1 className={classes.value}>{count}</h1>
+            <button className={classes.button} onClick={increment}><span>
                 asdf
                 </span></button>
             Hello world!
